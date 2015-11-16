35d3c7b9dfc3c7475bbc0834994070a67d3c3922
ronsnow <ronsnow@ProDad.local>
2015-11-15 20:21:38 -0800

    Second commit

--- lib/uxunity.rb
+++ lib/uxunity.rb
@@ -1,4 +1,8 @@
 require "uxunity/version"
+require "compass"
+
+base_directory = File.join(File.dirname(__FILE__))
+Compass::Frameworks.register('uxunity', :path => base_directory)
 
 module Uxunity
   # Your code goes here...
